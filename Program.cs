db5e98891d42fe8feed1fc54646cb9a32418e078
Dagg <theamazingdoge91@gmail.com>
2019-07-20 05:39:36 -0700

    Foxes, githubs, galore!

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using Discord;
 using Discord.WebSocket;
 using Discord.Commands;
 
-
 namespace foxbot
 {
     /* 
@@ -14,10 +13,11 @@ namespace foxbot
     hey its like npm install, npm start!
     */ 
 
-    class Program
+    class Program : ModuleBase<SocketCommandContext>
     {
         private DiscordSocketClient _client;
         public Random rnd = new Random();
+
         static void Main(string[] args)
         {
             new Program().MainAsync().GetAwaiter().GetResult();
@@ -26,25 +26,65 @@ namespace foxbot
         public async Task MainAsync()
         {
             _client = new DiscordSocketClient();
-            await _client.LoginAsync(TokenType.Bot, "TOKEN");
+            _client.Log += Log;
+
+            await _client.LoginAsync(TokenType.Bot, "EDIT.TOKEN");
             _client.MessageReceived += MessageReceived;
+
             await _client.StartAsync();
+
             await Task.Delay(-1);
         }
 
-        private async Task MessageReceived(SocketMessage message)
+        public async Task MessageReceived(SocketMessage message)
         {
             // Extremely basic "command" handler
             switch(message.Content)
             {
-                case "!hello":
-                    await message.Channel.SendMessageAsync("Hello!");
+                #region !about
+                case "!about":
+                    var aboutembed = new EmbedBuilder();
+                    aboutembed.WithAuthor("FoxBot", "https://cdn.discordapp.com/avatars/601967284394917900/f25955e890f89f1015762647f82ea555.webp")
+                    .WithThumbnailUrl("https://dagg.xyz/randomfox/images/" + rnd.Next(0, 125) + ".jpg")
+                    .WithTitle("Github")
+                    .WithColor(255,0,100)
+                    .WithUrl("https://github.com/daggintosh/foxbox")
+                    .WithDescription("**Hello!**");
+                    await message.Channel.SendMessageAsync("", false, aboutembed.Build());
                     break;
+                #endregion
                 
+                #region !fox
                 case "!fox":
-                    await message.Channel.SendMessageAsync("https://dagg.xyz/randomfox/images/" + rnd.Next(0, 125) + ".jpg");
+                    var foxembed = new EmbedBuilder();
+
+                    #region Catchphrases
+                    string[] catchphrase = {"A fox appears!", 
+                    "A fox is here!", 
+                    "Theres a fox here!", 
+                    "A fox has manifested!",
+                    "A fox has taken hold!",
+                    "There's a fox in my boot!",
+                    "A wild fox has appeared!",
+                    "A fox challenges you!",
+                    "You see a fox!",
+                    "Wow! A fox!"};
+                    #endregion
+
+                    foxembed.WithAuthor("FoxBot", "https://cdn.discordapp.com/avatars/601967284394917900/f25955e890f89f1015762647f82ea555.webp")
+                    .WithImageUrl("https://dagg.xyz/randomfox/images/" + rnd.Next(0, 125) + ".jpg")
+                    .WithDescription(catchphrase[rnd.Next(catchphrase.Length)])
+                    .WithColor(rnd.Next(0,255), rnd.Next(0,255), rnd.Next(0,255));
+                    await message.Channel.SendMessageAsync("", false, foxembed.Build());
                     break;
+                #endregion
             }
+        }
+
+        private Task Log(LogMessage msg)
+        {
+	        Console.WriteLine(msg.ToString());
+	        return Task.CompletedTask;
         }
     }
 }
